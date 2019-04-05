2e7b8bceee87554f0c939e8b028b6b21ab263fa9
Jack Maguire <jack@med.unc.edu>
2019-04-05 14:50:52 -0400

    finished first write-through

--- training/train.py
+++ training/train.py
@@ -56,9 +56,9 @@ parser.add_argument( "--training_data", help="CSV where each line has two elemen
 # /home/jack/input.3.csv,/home/jack/output.3.csv
 # ...
 
-parser.add_argument( "--starting_epochs", help="For bookkeeping purposes, what is the epoch number of the model loaded with --model?", type=int, required=True )
+parser.add_argument( "--starting_epoch", help="For bookkeeping purposes, what is the epoch number of the model loaded with --model?", type=int, required=True )
 parser.add_argument( "--epoch_checkpoint_frequency", help="How often should we be saving models?", type=int, required=True )
-parser.add_argument( "--num_epochs", help="Number of epochs to run. 0 means infinite loop.", type=int, required=True )
+parser.add_argument( "--num_epochs", help="Number of epochs to run.", type=int, required=True )
 
 #args = parser.parse_args()
 
@@ -109,6 +109,8 @@ def generate_data_from_files( filenames_csv ):
     my_assert_equals( "len( input_no_resid[ 0 ] )", len( input_no_resid[ 0 ] ), num_input_dimensions );
     my_assert_equals( "len( output_no_resid[ 0 ] )", len( output_no_resid[ 0 ] ), num_output_dimensions );
 
+    #https://www.kaggle.com/vishwasgpai/guide-for-creating-cnn-model-using-csv-file
+
     return input_no_resid, output_no_resid
 
 generate_data_from_files( "../sample_data/sample.repack.input.csv,../sample_data/sample.repack.output.csv" )
@@ -135,88 +137,33 @@ def mean_pred( y_true, y_pred ):
 # START #
 #########
 
-# 1) Define Filenames
-#training_input_file = "training.dat"
-#testing_input_file = "testing.dat"
 
-#training_input, training_output_hbond = generate_data_from_file( training_input_file )
-#testing_input, testing_output_hbond = generate_data_from_file( testing_input_file )
-
-training_input = numpy.load( "training.dat.input.npy" )
-training_output_hbond = numpy.load( "training.dat.hbond.npy" )
-
-testing_input = numpy.load( "testing.dat.input.npy" )
-testing_output_hbond = numpy.load( "testing.dat.hbond.npy" )
-
-nonnative_training_input = numpy.load( "nonnative.input.npy" )
-nonnative_training_hbond = numpy.load( "nonnative.hbond.npy" )
-
-native_testing_input, native_testing_output_hbond = generate_data_from_file( "native.csv" )
-
-weight1 = calc_weight( nonnative_training_hbond, training_output_hbond )
-print( "weight: " + str( weight1 ) )
-
-# 2) Define Model
-
-if os.path.isfile( "best.h5" ):
+if os.path.isfile( args.model ):
     model = load_model( "best.h5" )
 else:
-    num_input_dimensions = 9
-    model = Sequential()
-
-    model.add( Dense( num_neurons_in_first_hidden_layer, input_dim=num_input_dimensions, activation='relu') )
-
-    for x in range( 0, num_intermediate_hidden_layers ):
-        model.add( Dense( num_neurons_in_intermediate_hidden_layer, activation='relu') )
-
-    num_neurons_in_final_layer = int( 1 )
-    model.add( Dense( num_neurons_in_final_layer, activation='sigmoid') )
-
-    # 3) Compile Model
-
-    metrics_to_output=[ 'accuracy' ]
-    model.compile( loss='binary_crossentropy', optimizer='adam', metrics=metrics_to_output )
+    print( "Model " + args.model + " is not a file" )
+    exit( 1 )
 
 # 4) Fit Model
-best_score_so_far = 0
-test_ppv, test_npv = evaluate_model( model, best_score_so_far, testing_input, testing_output_hbond, 0 )
-native_ppv, native_npv = evaluate_model( model, best_score_so_far, native_testing_input, native_testing_output_hbond, 0 )
-best_score_so_far = score( test_ppv, test_npv, native_ppv, native_npv )
-print( "0 " + str( test_ppv ) + " " +  str( test_npv ) + " " +  str( native_ppv ) + " " +  str( native_npv ) )
+starting_epoch = args.starting_epoch
+last_epoch = starting_epoch + args.num_epochs
 
-for x in range( 0, num_epochs ):
-    #start = time.time()
-    #print( "Beginning epoch: " + str(x) )
+save_frequency = args.epoch_checkpoint_frequency
+epochs_until_next_save = args.epoch_checkpoint_frequency
+
+for epoch in range( starting_epoch + 1, last_epoch + 1 ):
+
+    file = open( args.training_data, "r" )
+
+    for line in file:
+        input, output = generate_data_from_files( line )
+        model.train_on_batch( x=input, y=output )
+
+    file.close()
     
-    shuffle_in_unison( training_input, training_output_hbond )
-    i=0
-    while i < len(training_input):
-        j = len(training_input) - i
-        if j >  100000:
-            j = 100000
-        i +=    100000
-
-        model.train_on_batch( x=training_input[ i : i+j ], y=training_output_hbond[ i : i+j ], class_weight={ 0 : 1, 1 : weight1 } )
-
-    shuffle_in_unison( nonnative_training_input, nonnative_training_hbond )
-    i=0
-    while i < len(nonnative_training_input):
-        j = len(nonnative_training_input) - i
-        if j >  10000:
-            j = 10000
-        i +=    10000
-
-        model.train_on_batch( x=nonnative_training_input[ i : i+j ], y=nonnative_training_hbond[ i : i+j ], class_weight={ 0 : 1, 1 : weight1 } )
-
-    if ( x % 10 == 0 ):
-        model.save( "epoch_" + str(x) + ".h5" )
-        test_ppv, test_npv = evaluate_model( model, best_score_so_far, testing_input, testing_output_hbond, 0 )
-        native_ppv, native_npv = evaluate_model( model, best_score_so_far, native_testing_input, native_testing_output_hbond, 0 )
-        best_score_so_far = score( test_ppv, test_npv, native_ppv, native_npv )
-        print( str( x ) + " " + str( test_ppv ) + " " +  str( test_npv ) + " " +  str( native_ppv ) + " " +  str( native_npv ) + " " + str( best_score_so_far ) )
-
-    #end = time.time()
-    #print( "\tseconds: " + str( end - start ) )
-    sys.stdout.flush()
+    epochs_until_next_save = epochs_until_next_save - 1
+    if ( epochs_until_next_save == 0 ):
+        epochs_until_next_save = save_frequency
+        model.save( "epoch_" + str( epoch ) + ".h5" )
 
 model.save( "final.h5" )
