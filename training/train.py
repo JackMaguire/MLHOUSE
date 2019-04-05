c4ecbd6624c954888c014db7deec081104f3a39f
Jack Maguire <jack@med.unc.edu>
2019-04-05 11:06:51 -0400

    removing error

--- training/train.py
+++ training/train.py
@@ -1,6 +1,6 @@
-1;95;0cimport os
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 #from keras import *
 from keras.models import Sequential
