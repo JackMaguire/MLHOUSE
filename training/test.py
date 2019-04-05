1946e0bcc47fa76d74aecc4df7980e94dbcda120
Jack Maguire <jack@med.unc.edu>
2019-04-05 15:57:07 -0400

    test.py

---
+++ training/test.py
@@ -0,0 +1,144 @@
+import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+#from keras import *
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import metrics
+
+from keras.models import load_model
+import keras.backend as K
+import keras.callbacks
+import keras
+import numpy
+
+import sys
+#sys.path.append("/nas/longleaf/home/jackmag")#for h5py
+import h5py
+
+import pandas as pd
+
+import argparse
+import random
+import time
+import subprocess
+
+########
+# INIT #
+########
+
+num_input_dimensions = 20021
+num_neurons_in_layer1 = 4000
+num_neurons_in_layer2 = 2000
+num_neurons_in_layer3 = 500
+num_neurons_in_layer4 = 100
+num_output_dimensions = 2
+
+numpy.random.seed( 0 )
+
+#Get sha1
+pwd = os.path.realpath(__file__)
+MLHOUSE_index = pwd.find( "MLHOUSE" )
+path = pwd[:MLHOUSE_index]
+full_name = "~/MLHOUSE/.git".replace( "~", path )
+sha1 = subprocess.check_output(["git", "--git-dir", full_name, "rev-parse", "HEAD"]).strip()
+print ( "JackMaguire/MLHOUSE: " + str( sha1 ) )
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument( "--model", help="Most recent model file", required=True )
+
+parser.add_argument( "--testing_data", help="CSV where each line has two elements. First element is the absolute path to the input csv file, second element is the absolute path to the corresponding output csv file.", required=True )
+# Example: "--training_data foo.csv" where foo.csv looks like:
+# /home/jack/input.1.csv,/home/jack/output.1.csv
+# /home/jack/input.2.csv,/home/jack/output.2.csv
+# /home/jack/input.3.csv,/home/jack/output.3.csv
+# ...
+
+args = parser.parse_args()
+
+#########
+# FUNCS #
+#########
+
+def my_assert_equals( name, actual, theoretical ):
+    if actual != theoretical:
+        print( str( name ) + " is equal to " + str( actual ) + " instead of " + str( theoretical ) )
+        exit( 1 )
+
+
+def assert_vecs_line_up( input, output ):
+    #Each line starts with "RESID: XXX,"
+    my_assert_equals( "input file length", len( input ), len( output ) )
+    for i in range( 0, len( input ) ):
+        in_elem = input[ i ][ 0 ]
+        in_resid = int( in_elem.split( " " )[ 1 ] )
+        out_elem = output[ i ][ 0 ]
+        out_resid = int( out_elem.split( " " )[ 1 ] )
+        my_assert_equals( "out_resid", out_resid, in_resid )
+
+def generate_data_from_files( filenames_csv ):
+    #dataset = numpy.genfromtxt( filename, delimiter=",", skip_header=0 )
+    split = filenames_csv.split( "\n" )[ 0 ].split( "," );
+    my_assert_equals( "split.length", len( split ), 2 );
+
+    # Both of these elements lead with a dummy
+    input  = pd.read_csv( split[ 0 ] ).values
+    output = pd.read_csv( split[ 1 ] ).values
+
+    assert_vecs_line_up( input, output )
+
+    input_no_resid = input[:,1:]
+    output_no_resid = output[:,1:]
+
+    my_assert_equals( "len( input_no_resid[ 0 ] )", len( input_no_resid[ 0 ] ), num_input_dimensions );
+    my_assert_equals( "len( output_no_resid[ 0 ] )", len( output_no_resid[ 0 ] ), num_output_dimensions );
+
+    #https://www.kaggle.com/vishwasgpai/guide-for-creating-cnn-model-using-csv-file
+
+    return input_no_resid, output_no_resid
+
+#generate_data_from_files( "../sample_data/sample.repack.input.csv,../sample_data/sample.repack.output.csv" )
+#exit( 0 )
+
+###########
+# CLASSES #
+###########
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
+###########
+# METRICS #
+###########
+
+def mean_pred( y_true, y_pred ):
+    return K.mean( y_pred )
+
+#########
+# START #
+#########
+
+
+if os.path.isfile( args.model ):
+    model = load_model( args.model )
+else:
+    print( "Model " + args.model + " is not a file" )
+    exit( 1 )
+
+# 4) Test Model
+for epoch in range( starting_epoch + 1, last_epoch + 1 ):
+
+    file = open( args.training_data, "r" )
+
+    for line in file:
+        input, output = generate_data_from_files( line )
+        results = model.evaluate( x=input, y=output )
+        print( results )
+
+    file.close()
+
