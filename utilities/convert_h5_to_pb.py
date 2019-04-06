c08d4c532ec3fcec32b6bcc155f126dc9641489f
Jack Maguire <jack@med.unc.edu>
2019-04-05 20:47:17 -0400

    convert_h5_to_pb.py

---
+++ utilities/convert_h5_to_pb.py
@@ -0,0 +1,109 @@
+import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+#from keras import *
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import metrics
+
+from keras.models import load_model
+import keras.backend as K
+import keras.callbacks
+import keras
+import numpy
+
+import sys
+#sys.path.append("/nas/longleaf/home/jackmag")#for h5py
+import h5py
+
+import pandas as pd
+
+import argparse
+import random
+import time
+import subprocess
+
+from keras import backend as K
+
+########
+# INIT #
+########
+
+num_input_dimensions = 20021
+num_neurons_in_layer1 = 4000
+num_neurons_in_layer2 = 2000
+num_neurons_in_layer3 = 500
+num_neurons_in_layer4 = 100
+num_output_dimensions = 2
+
+numpy.random.seed( 0 )
+
+#Get sha1
+pwd = os.path.realpath(__file__)
+MLHOUSE_index = pwd.find( "MLHOUSE" )
+path = pwd[:MLHOUSE_index]
+full_name = "~/MLHOUSE/.git".replace( "~", path )
+sha1 = subprocess.check_output(["git", "--git-dir", full_name, "rev-parse", "HEAD"]).strip()
+print ( "JackMaguire/MLHOUSE: " + str( sha1 ) )
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument( "--model", help="Most recent model file", required=True )
+parser.add_argument( "--out", help="Filename for result", required=True )
+
+parser.add_argument( "--evaluate_individually", help="Print predictions vs actual data", type=bool, default=False )
+
+parser.add_argument( "--testing_data", help="CSV where each line has two elements. First element is the absolute path to the input csv file, second element is the absolute path to the corresponding output csv file.", required=True )
+# Example: "--testing_data foo.csv" where foo.csv looks like:
+# /home/jack/input.1.csv,/home/jack/output.1.csv
+# /home/jack/input.2.csv,/home/jack/output.2.csv
+# /home/jack/input.3.csv,/home/jack/output.3.csv
+# ...
+
+args = parser.parse_args()
+
+# https://stackoverflow.com/questions/45466020/how-to-export-keras-h5-to-tensorflow-pb
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
+
+        
+#########
+# START #
+#########
+
+
+if os.path.isfile( args.model ):
+    model = load_model( args.model )
+else:
+    print( "Model " + args.model + " is not a file" )
+    exit( 1 )
+
+
+frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name for out in model.outputs])
+tf.train.write_graph(frozen_graph, ".", args.out, as_text=False)
