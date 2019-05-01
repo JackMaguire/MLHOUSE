3fd9b503ad1f2e4c6a09ce5d275e04bf80f78b62
Jack Maguire <jackmaguire1444@gmail.com>
2019-05-01 18:55:57 -0400

    Added leaky relu options

---
+++ training/first_generation_of_candidate_models/rrrrl.1000.250.50.10.leaky.py
@@ -0,0 +1,67 @@
+import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+#from keras import *
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LeakyReLU
+from keras import metrics
+
+from keras.models import load_model
+import keras.backend as K
+import keras.callbacks
+import keras
+import numpy
+
+import sys
+#sys.path.append("/nas/longleaf/home/jackmag")#for h5py
+import h5py
+
+import argparse
+import random
+import time
+import subprocess
+
+########
+# INIT #
+########
+
+numpy.random.seed( 0 )
+
+#Get sha1
+pwd = os.path.realpath(__file__)
+MLHOUSE_index = pwd.find( "MLHOUSE" )
+path = pwd[:MLHOUSE_index]
+full_name = "~/MLHOUSE/.git".replace( "~", path )
+sha1 = subprocess.check_output(["git", "--git-dir", full_name, "rev-parse", "HEAD"]).strip()
+print ( "JackMaguire/MLHOUSE: " + str( sha1 ) )
+
+parser = argparse.ArgumentParser()
+parser.add_argument( "--model", help="filename for output file", default="blank_model", required=False )
+args = parser.parse_args()
+
+
+num_input_dimensions = 20021
+num_neurons_in_layer1 = 1000
+num_neurons_in_layer2 = 250
+num_neurons_in_layer3 = 50
+num_neurons_in_layer4 = 10
+num_output_dimensions = 2
+model = Sequential()
+
+model.add( Dense( num_neurons_in_layer1, input_dim=num_input_dimensions, activation='relu') )
+model.add( LeakyReLU(alpha=.01) )
+model.add( Dense( num_neurons_in_layer2, activation='relu') )
+model.add( LeakyReLU(alpha=.01) )
+model.add( Dense( num_neurons_in_layer3, activation='relu') )
+model.add( LeakyReLU(alpha=.01) )
+model.add( Dense( num_neurons_in_layer4, activation='relu') )
+model.add( LeakyReLU(alpha=.01) )
+model.add( Dense( num_output_dimensions, activation='linear') )
+
+# 3) Compile Model
+
+metrics_to_output=[ 'accuracy' ]
+model.compile( loss='mean_squared_error', optimizer='adam', metrics=metrics_to_output )
+model.save( args.model + ".h5" )
