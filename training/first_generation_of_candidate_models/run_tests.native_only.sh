87339a12eab9933b6333d08cdd628e15a7e8b620
Jack Maguire <jackmaguire1444@gmail.com>
2019-05-21 16:17:53 -0400

    run_tests.david_only.sh run_tests.native_only.sh first_gen_loop.sh

---
+++ training/first_generation_of_candidate_models/run_tests.native_only.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+prefix=$1
+subrange=$2
+if [ ! -f $prefix.trainingset3.epoch1.subrange$subrange.h5 ]; then
+    echo "$prefix.trainingset3.epoch1.subrange$subrange.h5 does not exist"
+    exit 1
+fi
+
+x=$subrange
+
+for y in 2.onesided.csv 2.random.csv 2.repack.csv 2.twosided.csv; do
+
+    if grep $y $prefix.trainingset3.epoch1.subrange$x.testlog; then
+	echo Skipping $y
+	continue
+    fi
+
+    echo python3 ../test.custom1.py --model $prefix.trainingset3.epoch1.subrange$x.h5 --testing_data /home/jackmag/mlhouse_training_data/testing_data/npy_training_data.testing_set_$y
+    echo $y `python3 ../test.custom1.py --model $prefix.trainingset3.epoch1.subrange$x.h5 --testing_data /home/jackmag/mlhouse_training_data/testing_data/npy_training_data.testing_set_$y 2>&1 | grep RESULTS` >> $prefix.trainingset3.epoch1.subrange$x.testlog
+done
+
