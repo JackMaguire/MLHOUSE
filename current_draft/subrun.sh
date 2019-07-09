15700ac81484a653d5077a1439b8ae782b941ae7
Jack Maguire <jackmaguire1444@gmail.com>
2019-07-09 08:35:07 -0400

    5d6260eb suffix for tar files

--- current_draft/subrun.sh
+++ current_draft/subrun.sh
@@ -2,21 +2,21 @@
 
 setup_next_dir(){
     next_dir_num=$1
-    if [ ! -f /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.tar.gz ]; then
-	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.tar.gz /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.tar.gz
+    if [ ! -f /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz ]; then
+	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.5d6260eb.tar.gz /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz
     fi
-    ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.tar.gz
+    ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz
 
 #    if [[ "$next_dir_num" -lt "0" ]]; then
 #	#local
-#	ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.tar.gz
+#	ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz
 #    else
 #	#scp
-#	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.tar.gz .
+#	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.5d6260eb.tar.gz .
 #    fi
-    tar -xzf $next_dir_num.tar.gz
+    tar -xzf $next_dir_num.5d6260eb.tar.gz
     mv $next_dir_num next
-    rm $next_dir_num.tar.gz
+    rm $next_dir_num.5d6260eb.tar.gz
 }
 
 next_dir=$1
