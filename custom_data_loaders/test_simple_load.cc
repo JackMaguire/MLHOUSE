ea9bdef42e1cea566e0fe170b8275a3c05c5e251
Jack Maguire <jackmaguire1444@gmail.com>
2019-07-09 13:03:55 -0400

    test_simple_load.cc

--- custom_data_loaders/test_simple_load.cc
+++ custom_data_loaders/test_simple_load.cc
@@ -11,10 +11,7 @@
 
 namespace py = pybind11;
 
-template <class T>
-using ndarray_tmpl = xt::pyarray<T, xt::layout_type::row_major>;
-
-using ndarray = ndarray_tmpl< float >;
+using ndarray = xt::pyarray< float >;
 
 namespace mouse_io {
 
@@ -32,34 +29,34 @@ normalize_output_value(
 
 ndarray
 generate_output_data(
-  std::vector< std::array< std::string, 3 > > const & tokenized_file_lines_of_output_file
+  std::vector< std::array< std::string, 2 > > const & tokenized_file_lines_of_output_file
 ){
   int const total_number_of_elements = tokenized_file_lines_of_output_file.size();
 
   ndarray output_values = xt::zeros< float >({total_number_of_elements});//TODO use better ctor
 
   for( int i = 0; i < total_number_of_elements; ++i ){
-    output_values[ i ] = std::stof( tokenized_file_lines_of_output_file[ 1 ] );
+    output_values[ i ] = std::stof( tokenized_file_lines_of_output_file[ i ][ 1 ] );
   }
 
   return output_values;
 }
 
 //ndarray
-std::vector< std::array< std::string, 3 > >
+std::vector< std::array< std::string, 2 > >
 read_in_output_data(
   std::string const & filename
 ) {
   //https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
   //https://stackoverflow.com/questions/11719538/how-to-use-stringstream-to-separate-comma-separated-strings
-  std::vector< std::array< std::string, 3 > > tokenized_file_lines_of_output_file;
+  std::vector< std::array< std::string, 2 > > tokenized_file_lines_of_output_file;
   std::ifstream infile( filename );
   std::string line;
   while( std::getline( infile, line, ',' ) ) {
     std::istringstream iss( line );
     std::string resid_tag;
-    float energy;
-    float ddg;
+    std::string energy;
+    std::string ddg;
 
     if ( !( iss >> resid_tag >> energy >> ddg ) ) {
       // error
@@ -67,16 +64,27 @@ read_in_output_data(
       continue;//for now, just omit this line
     }
 
-    if( resid_tag.substr( 0, "RESID: ".size() ) != "RESID: " ){
+    /*
+    //"RESID: ".size() == 7
+    constexpr int resid_tag_prefix_size = 7;
+
+    if( resid_tag.substr( 0, resid_tag_prefix_size ) != "RESID: " ){
       // error
       // TODO - learn how to best handle errors in python
       continue;//for now, just omit this line      
     }
 
     //remove "RESID: " from beginning of tag and parse it as an int
-    int resid = std::stoi( resid_tag.substr( "RESID: ".size() ) );
+    int resid = std::stoi( resid_tag.substr( resid_tag_prefix_size ) );
+*/
+    //Can't seem to get emplace back to work. 
+    //Let's just go the slow way for now
+    std::array< std::string, 2 > new_element;
+    new_element[ 0 ] = resid_tag;
+    new_element[ 1 ] = energy;
+    //new_element[ 2 ] = ddg;
 
-    tokenized_file_lines_of_output_file.emplace_back( float( resid ), energy, ddg );
+    tokenized_file_lines_of_output_file.emplace_back( new_element );
   }
 
   // infile.close(); // Let RAII handle this
@@ -103,4 +111,4 @@ PYBIND11_MODULE( example, m ) {
   m.def("read_mouse_data", &mouse_io::read_mouse_data, "TODO");
 }
 
-//c++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` example.cpp -o example`python3-config --extension-suffix
+//c++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` example.cpp -o example`python3-config --extension-suffix`
