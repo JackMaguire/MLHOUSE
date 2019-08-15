8b1d30745d4b97123e0c1affb3009557b2308774
Jack Maguire <jackmaguire1444@gmail.com>
2019-08-15 12:03:37 -0400

    lr

--- multicool_draft/subrun.sh
+++ multicool_draft/subrun.sh
@@ -21,7 +21,8 @@ setup_next_dir(){
 }
 
 next_dir=$1
-command=$2
+lr=$2
+command=$3
 
 if [[ $command -eq 1 ]]; then
     #setup next dir
@@ -40,7 +41,7 @@ else
     else
 	for x in ../sub_trains/*.py; do
 	    echo $x
-	    ( python3 $x --training_data local_list.csv 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
+	    ( python3 $x --training_data local_list.csv --learning_rate $lr 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
 	done
     fi
     #python3 ../train.py --training_data local_list.csv 2>/dev/null | tail -n 3
