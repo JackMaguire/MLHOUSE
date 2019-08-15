37a8ef3704f015a387eea1db6c23c9058a26a39e
Jack Maguire <jackmaguire1444@gmail.com>
2019-08-15 10:09:46 -0400

    subrun.sh run.sh

---
+++ multicool_draft/run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# We have 2 directories: curr and next
+
+# 0. make first dir (create next, move to curr)
+# loop:
+#     1. run on curr WHILE create next
+#     2. delete curr
+#     3. move next to curr
+
+workdir=`pwd`
+
+epoch=1
+for y in start.*.h5; do
+    cp $y `echo $y | sed 's/start/current/g'`
+done
+
+get_next_dir_num(){
+    ssh jackmag@rosettadesign.med.unc.edu bash get_a_dir.sh
+}
+
+setup_next_dir(){
+    next_dir_num=$1
+    bash subrun.sh $next_dir_num 1
+}
+
+# Clean up from previous attempt
+rm -r curr next
+
+# 0
+next_dir=`get_next_dir_num`
+setup_next_dir $next_dir
+if [ ! -d next ]; then
+    echo "next does not exist"
+    exit 1
+fi
+mv next curr
+
+for epoch in {0..1000}; do
+
+    cd $workdir
+    if ls epoch_${epoch}.*.h5 1>/dev/null 2>/dev/null; then
+	echo "Skipping epoch $epoch"
+	for y in epoch_$epoch.*.h5; do
+	    cp $y `echo $y | sed "s/epoch_$epoch/current/g"`
+	done
+	continue;
+    fi
+
+    echo "Starting epoch $epoch"
+
+    for subepoch in {1..10}; do
+	next_dir=`get_next_dir_num`
+
+	#At this moment, curr is ready to run and next does not exist
+
+	# 1
+	echo "START " `date`
+	echo 1 2 | xargs -n 1 -P 2 bash subrun.sh $next_dir
+	if [[ `ls final.*.h5 | wc -l` -ne `ls current.*.h5 | wc -l` ]]; then
+	    echo "finals where not created"
+	    ls
+	    exit 1
+	fi
+	for y in final.*.h5; do
+	    mv $y `echo $y | sed 's/final/current/g'`
+	done
+
+	# 2
+	rm -r curr
+	if [ -d curr ]; then
+	    echo "curr was not properly deleted"
+	    exit 1
+	fi
+
+	# 3
+	if [ ! -d next ]; then
+	    echo "next does not exist"
+	    exit 1
+	fi
+	mv next curr
+
+    done
+
+    for y in current.*.h5; do
+	cp $y `echo $y | sed "s/current/epoch_$epoch/g"`
+    done
+done
---
+++ multicool_draft/subrun.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+setup_next_dir(){
+    next_dir_num=$1
+    if [ ! -f /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz ]; then
+	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.5d6260eb.tar.gz .
+    else
+	ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz
+    fi
+
+#    if [[ "$next_dir_num" -lt "0" ]]; then
+#	#local
+#	ln -s /home/jackmag/top8000_mousetrap/contador_overflow/$next_dir_num.5d6260eb.tar.gz
+#    else
+#	#scp
+#	scp jackmag@rosettadesign.med.unc.edu:~/mlhouse_training_data/training_data/$next_dir_num.5d6260eb.tar.gz .
+#    fi
+    tar -xzf $next_dir_num.5d6260eb.tar.gz
+    mv $next_dir_num next
+    rm $next_dir_num.5d6260eb.tar.gz
+}
+
+next_dir=$1
+command=$2
+
+if [[ $command -eq 1 ]]; then
+    #setup next dir
+    next_dir_num=$next_dir
+    setup_next_dir $next_dir_num
+    echo "DONE SETUP " `date`
+else
+    #run on current dir
+    cd curr
+    if false ; then
+	head -n10 local_list.csv > local_list.10.csv
+	for x in ../sub_trains/*.py; do
+	    echo $x
+	    ( python3 $x --training_data local_list.10.csv 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
+	done
+    else
+	for x in ../sub_trains/*.py; do
+	    echo $x
+	    ( python3 $x --training_data local_list.csv 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
+	done
+    fi
+    #python3 ../train.py --training_data local_list.csv 2>/dev/null | tail -n 3
+    mv final.*.h5 ../
+    echo "DONE TRAIN " `date`
+fi
