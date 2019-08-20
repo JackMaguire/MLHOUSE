eb9365c286856b75ab7ea3377b0ef0f0bea3685e
Jack Maguire <jackmaguire1444@gmail.com>
2019-08-20 07:59:42 -0400

    create.base.high_channel.smallestdeep.partiallyleaky.py

--- multicool_draft/subrun.sh
+++ multicool_draft/subrun.sh
@@ -36,7 +36,7 @@ else
 	head -n10 local_list.csv > local_list.10.csv
 	for x in ../sub_trains/*.py; do
 	    echo $x
-	    ( python3 $x --training_data local_list.10.csv 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
+	    ( python3 $x --training_data local_list.10.csv --learning_rate $lr 2>/dev/null | tail -n 3 | head -n 1 ) || ( echo $x went bad && exit 1 )
 	done
     else
 	for x in ../sub_trains/*.py; do
