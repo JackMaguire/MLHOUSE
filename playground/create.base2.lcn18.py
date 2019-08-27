18b8b8515637b905109d2d1888bde2e4ea5bec89
Jack Maguire <jackmaguire1444@gmail.com>
2019-08-27 09:30:15 -0400

    keeping original 19 values for now

--- playground/create.base2.lcn18.py
+++ playground/create.base2.lcn18.py
@@ -43,11 +43,11 @@ a_val=0.25
 numpy.random.seed( 0 )
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "--model", help="filename for output file", default="start.base2.lcn", required=False )
+parser.add_argument( "--model", help="filename for output file", default="start.base2.lcn18", required=False )
 args = parser.parse_args()
 
 num_input_dimensions1 = 26
-num_input_dimensions2 = 18494 - num_input_dimensions1
+num_input_dimensions2 = 17496 #(18494 - num_input_dimensions1) * (18.0 / 19.0)
 
 input1 = Input(shape=(num_input_dimensions1,), name="in1", dtype="float32" )
 in1dense1 = Dense( name="in1dense1", units=50 )( input1 )
@@ -64,12 +64,12 @@ in1up = UpSampling3D( size=(18,9,1), data_format='channels_last' )( pre1 )
 input2 = Input(shape=(num_input_dimensions2,), name="in2", dtype="float32" )
 
 # Phase 1: in2 -> ABCDE
-pre = Reshape( target_shape=(36, 19, 27,) )( input2 )#ALWAYS DO WIDTH, HEIGHT, CHANNELS
+pre = Reshape( target_shape=(36, 18, 27,) )( input2 )#ALWAYS DO WIDTH, HEIGHT, CHANNELS
 A = Conv2D( name="layerA", filters=30, kernel_size=(1,1), padding='valid',   data_format='channels_last', activation='relu', use_bias=True )( pre )
 B1 = Conv2D( name="layerB1", filters=20, kernel_size=(1,1), padding='valid', data_format='channels_last', activation='relu', use_bias=True )( A )
 B2 = Conv2D( name="layerB2", filters=20, kernel_size=(1,1), padding='valid', data_format='channels_last', activation='relu', use_bias=True )( B1 )
 B3 = Conv2D( name="layerB3", filters=12, kernel_size=(1,1), padding='valid', data_format='channels_last', activation='relu', use_bias=True )( B2 )
-B4 = LocallyConnected2D( name="layerB4", filters=15, kernel_size=(2,3), strides=(2,2), padding='valid', data_format='channels_last', activation='relu', use_bias=True )( B3 )
+B4 = LocallyConnected2D( name="layerB4", filters=15, kernel_size=(2,2), strides=(2,2), padding='valid', data_format='channels_last', activation='relu', use_bias=True )( B3 )
 merge = tensorflow.keras.layers.concatenate( [B4,in1up], name="merge", axis=-1 )
 
 # Phase 2: FGHIJ
