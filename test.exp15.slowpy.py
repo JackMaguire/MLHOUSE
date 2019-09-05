c3b0657658c7250b1f8f1d6dfad8a87e9bd14346
Jack Maguire <jackmaguire1444@gmail.com>
2019-09-05 14:27:10 -0400

    2x speedup

--- test.exp15.slowpy.py
+++ test.exp15.slowpy.py
@@ -1,58 +1,10 @@
-#import jack_mouse_test
-
-import os
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-#os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
-from random import shuffle
-
-#from tensorflow.keras import *
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import metrics
-
-import tensorflow.keras.losses
-
-from tensorflow.keras.models import load_model
-import tensorflow.keras.backend as K
-import tensorflow.keras.callbacks
-import tensorflow.keras
 import numpy
-
-import sys
-#sys.path.append("/nas/longleaf/home/jackmag")#for h5py
-import h5py
-
 import pandas as pd
-#import gzip
+import gzip
 import math
-
 import argparse
 import random
-
-#import threading
 import time
-import subprocess
-
-#from tensorflow.python.client import device_lib
-#print(device_lib.list_local_devices())
-
-import tensorflow as tf
-#from tensorflow.keras.backend.tensorflow_backend import set_session
-
-name="base2.lcn.sum"
-
-'''
-#Only use part of the GPU, from https://github.com/keras-team/keras/issues/4161
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
-#config.gpu_options.visible_device_list = "0"
-config.log_device_placement = True  # to log device placement (on which device the operation ran)
-                                    # (nothing gets printed in Jupyter, only if you run it standalone)
-sess = tf.Session(config=config)
-set_session(sess)  # set this TensorFlow session as the default session for Keras
-'''
 
 ########
 # INIT #
@@ -73,10 +25,8 @@ numpy.random.seed( 0 )
 
 parser = argparse.ArgumentParser()
 
-#parser.add_argument( "--model", help="Most recent model file", required=True )
-
-parser.add_argument( "--training_data", help="CSV where each line has two elements. First element is the absolute path to the input csv file, second element is the absolute path to the corresponding output csv file.", required=True )
-# Example: "--training_data foo.csv" where foo.csv looks like:
+parser.add_argument( "--data", help="CSV where each line has two elements. First element is the absolute path to the input csv file, second element is the absolute path to the corresponding output csv file.", required=True )
+# Example: "--data foo.csv" where foo.csv looks like:
 # /home/jack/input.1.csv,/home/jack/output.1.csv
 # /home/jack/input.2.csv,/home/jack/output.2.csv
 # /home/jack/input.3.csv,/home/jack/output.3.csv
@@ -118,7 +68,21 @@ def assert_vecs_line_up( input, output ):
         out_resid = int( out_elem.split( " " )[ 1 ] )
         my_assert_equals_thrower( "out_resid", out_resid, in_resid )
 
-def generate_data_from_files( filenames_csv, six_bin ):
+
+def denormalize_val( val ):
+    #print( "denromalizing ", val, " to ", (math.exp( math.exp( val + 1 ) ) - 10) )
+    #return math.exp( math.exp( val + 1 ) ) - 10;
+    if val <= -1.0:
+        val = -0.999
+    try:
+        i = math.log( val + 1.0 ) * -15.0
+    except:
+        print( val )
+        #print( e )
+        exit( 1 )
+    return i
+
+def generate_data_from_files_control( filenames_csv, six_bin ):
     #dataset = numpy.genfromtxt( filename, delimiter=",", skip_header=0 )
     split = filenames_csv.split( "\n" )[ 0 ].split( "," );
     my_assert_equals_thrower( "split.length", len( split ), 2 );
@@ -190,60 +154,59 @@ def generate_data_from_files_fast( filenames_csv, six_bin ):
         output[x][0] = math.exp( val / -15.0 ) - 1.0
     return source_input_no_resid, ray_input_no_resid, output
 
+
 #########
 # START #
 #########
 
-if os.path.isfile( "../current." + name + ".h5" ):
-    model1 = load_model( "../current." + name + ".h5" )
-else:
-    print( "Model ../current." + name + ".h5 is not a file" )
-    exit( 1 )
-
-#0.001 is default
-#model1.optimizer.lr = 0.00025
-model1.optimizer.lr = 0.005
-print( "lr:", model1.optimizer.lr )
-
-# 4) Fit Model
-
-with open( args.training_data, "r" ) as f:
+with open( args.data, "r" ) as f:
     file_lines = f.readlines()
-num_file_lines = len( file_lines )
 
-time_spent_loading = float( 0 )
-time_spent_training = float( 0 )
-
-shuffle( file_lines )
+t0 = time.time()
 
 for line in file_lines:
-    print( "reading " + str( line ) )
+    #print( "reading " + str( line ) )
     split = line.split( "\n" )[ 0 ].split( "," );
     my_assert_equals_thrower( "split.length", len( split ), 2 );
 
-    t0 = time.time()
     try:
-        '''
-        cpp_structs = jack_mouse_test.read_mouse_data( split[ 0 ], split[ 1 ] )
-        source_input = cpp_structs[ 0 ]
-        ray_input = cpp_structs[ 1 ]
-        output = cpp_structs[ 2 ]
-        '''
-        source_input, ray_input, output = generate_data_from_files_fast( line, False )
-        #print( output )
+        source_input1, ray_input1, output1 = generate_data_from_files_fast( line, False )
     except AssertError:
         continue
-    t1 = time.time()
-    #submitting in batches to save memory on the GPU
-    #64 sometimes works, 128 is too large
-    #model1.train_on_batch( x=[source_input,ray_input], y=output )
-    model1.fit( x=[source_input,ray_input], y=output, epochs=1, batch_size=32 )
-    t2 = time.time()
-    time_spent_loading += t1 - t0
-    time_spent_training += t2 - t1
+ 
+t1 = time.time()
+print( "time:", (t1-t0) )
 
-print( str( float( time_spent_loading ) / float(time_spent_loading + time_spent_training) ) + " fraction of time was spent loading" )
-print( time_spent_loading )
-print( time_spent_training )
 
-model1.save( "final." + name + ".h5" )
+t0 = time.time()
+
+for line in file_lines:
+    #print( "reading " + str( line ) )
+    split = line.split( "\n" )[ 0 ].split( "," );
+    my_assert_equals_thrower( "split.length", len( split ), 2 );
+
+    try:
+        source_input2, ray_input2, output2 = generate_data_from_files_control( line, False )
+    except AssertError:
+        continue
+ 
+t1 = time.time()
+print( "time:", (t1-t0) )
+
+for x in range(0,len(source_input1)):
+    for y in range( 0, len( source_input1[x] ) ):
+        if abs( source_input1[x][y] - source_input2[x][y] > 0.001 ):
+            print( "source_input is bad" )
+
+for x in range(0,len(ray_input1)):
+    for y in range( 0, len( ray_input1[x] ) ):
+        if abs( ray_input1[x][y] - ray_input2[x][y] > 0.001 ):
+            print( "ray_input is bad" )
+
+for x in range(0,len(output1)):
+    for y in range( 0, len( output1[x] ) ):
+        if abs( output1[x][y] - output2[x][y] > 0.001 ):
+            print( "output is bad" )
+
+
+#original: 116.92752194404602
